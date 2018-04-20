8f93e6dd422720feaa8c5aa113376539f213fb35
luchen <462326994@qq.com>
2018-04-20 14:31:10 +0800

    完成排行榜、高手榜、开奖记录接口

---
+++ Qiuxun.C8.Api.Service/Dtos/Leaderboard/RankIntegralResDto.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Qiuxun.C8.Api.Service.Dtos
+{
+    /// <summary>
+    /// 积分榜信息
+    /// </summary>
+    public class RankIntegralResDto
+    {
+        /// <summary>
+        /// 排名
+        /// </summary>
+        public int Rank { set; get; }
+
+        /// <summary>
+        /// 总积分
+        /// </summary>
+        public int Score { set; get; }
+
+        /// <summary>
+        /// 用户Id
+        /// </summary>
+        public int UserId { set; get; }
+
+        /// <summary>
+        /// 用户名称
+        /// </summary>
+        public string NickName { set; get; }
+
+        /// <summary>
+        /// 用户头像地址
+        /// </summary>
+        public string Avater { set; get; }
+    }
+}
