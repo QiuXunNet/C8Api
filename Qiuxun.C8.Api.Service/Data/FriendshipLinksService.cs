cc06a23c1aaa042c739387f41a4d71eac22f35ec
luchen <462326994@qq.com>
2018-04-26 14:40:35 +0800

    提交

---
+++ Qiuxun.C8.Api.Service/Data/FriendshipLinksService.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Qiuxun.C8.Api.Model;
+using Qiuxun.C8.Api.Public;
+
+namespace Qiuxun.C8.Api.Service.Data
+{
+    /// <summary>
+    /// PV EV  IP 保存
+    /// </summary>
+    public class FriendshipLinksService
+    {
+        public int GetLinkVisitRecordNum(int id)
+        {
+            string visitRecordSql = "select count(*) from dbo.LinkVisitRecord where RefId=@RefId and SubTime=@Date and [Type]=1";
+            //添加
+            var sqlParameter = new[]
+            {
+                        new SqlParameter("@RefId",id),
+                        new SqlParameter("@Date",DateTime.Today),
+                };
+            var count = Convert.ToInt32(SqlHelper.ExecuteScalar(visitRecordSql, sqlParameter));
+            return count;
+        }
+
+        /// <summary>
+        /// 添加Ip数量
+        /// </summary>
+        /// <param name="id"></param>
+        public void AddIp(int id)
+        {
+            var count = GetLinkVisitRecordNum(id);
+
+            if (count == 0)
+            {
+                //新增
+                string insertRecordSql = string.Format(
+                        @"insert into dbo.LinkVisitRecord (RefId,ClickCount,UV,IP,PV,RegCount,Type,SubTime)
+                        values({0},0,0,1,0,0,1,'{1}')", id, DateTime.Today);
+                SqlHelper.ExecuteScalar(insertRecordSql);
+
+            }
+            else
+            {
+                //修改
+                string updateRecordSql = string.Format(@"update dbo.LinkVisitRecord set IP+=1 where RefId={0} and SubTime = '{1}' and [Type]=1 ", id, DateTime.Today);
+                SqlHelper.ExecuteScalar(updateRecordSql);
+            }
+        }
+
+        /// <summary>
+        /// 添加PV数量
+        /// </summary>
+        /// <param name="id"></param>
+        public void AddPv(int id,int pvCount)
+        {
+            var count = GetLinkVisitRecordNum(id);
+
+            if (count == 0)
+            {
+                //新增
+                string insertRecordSql = string.Format(
+                        @"insert into dbo.LinkVisitRecord (RefId,ClickCount,UV,IP,PV,RegCount,Type,SubTime)
+                        values({0},0,0,0,{1},0,1,'{2}')", id, pvCount, DateTime.Today);
+                SqlHelper.ExecuteScalar(insertRecordSql);
+
+            }
+            else
+            {
+                //修改
+                string updateRecordSql = string.Format(@"update dbo.LinkVisitRecord set PV+={2} where RefId={0} and SubTime = '{1}' and [Type]=1 ", id, DateTime.Today, pvCount);
+                SqlHelper.ExecuteScalar(updateRecordSql);
+            }
+        }
+
+        /// <summary>
+        /// 添加UV数量
+        /// </summary>
+        /// <param name="id"></param>
+        public void AddUv(int id,int uvCount)
+        {
+            var count = GetLinkVisitRecordNum(id);
+
+            if (count == 0)
+            {
+                //新增
+                string insertRecordSql = string.Format(
+                        @"insert into dbo.LinkVisitRecord (RefId,ClickCount,UV,IP,PV,RegCount,Type,SubTime)
+                        values({0},0,{1},0,0,0,1,'{2}')", id, uvCount, DateTime.Today);
+                SqlHelper.ExecuteScalar(insertRecordSql);
+
+            }
+            else
+            {
+                //修改
+                string updateRecordSql = string.Format(@"update dbo.LinkVisitRecord set UV+={2} where RefId={0} and SubTime = '{1}' and [Type]=1 ", id, DateTime.Today, uvCount);
+                SqlHelper.ExecuteScalar(updateRecordSql);
+            }
+        }
+
+        /// <summary>
+        /// 查询所有友联
+        /// </summary>
+        /// <returns></returns>
+        public List<FriendLink> GetFriendLink()
+        {
+            var friendLinkList = Util.ReaderToList<FriendLink>("select * from dbo.FriendLink where [Type]=1 ");
+
+            return friendLinkList;
+        }
+    }
+}
---
+++ Qiuxun.C8.Api/Controllers/FriendshipLinksController.cs
@@ -0,0 +1,189 @@
+﻿using Qiuxun.C8.Api.Model;
+using Qiuxun.C8.Api.Service.Caching;
+using Qiuxun.C8.Api.Service.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Web.Http;
+
+namespace Qiuxun.C8.Api.Controllers
+{
+    /// <summary>
+    /// 友情链接接口
+    /// </summary>
+    public class FriendshipLinksController : ApiController
+    {
+        FriendshipLinksService _service = new FriendshipLinksService();
+        private static Timer sendTimer; //计时器
+        private static object objPv = new object(); //pv锁对象
+        private static object objUv = new object(); //uv锁对象
+
+        public FriendshipLinksController()
+        {
+            if (sendTimer == null)
+                sendTimer = new Timer(new TimerCallback(AddPvUv), this, 10000, 1 * 60 * 1000);
+        }
+
+        /// <summary>
+        /// 添加PV UV IP
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="linkCode"></param>
+        /// <param name="ip"></param>
+        [AllowAnonymous]
+        [HttpGet]
+        public void Add(string type, string linkCode, string ip = "")
+        {
+            FriendLink model = GetFriendLink(linkCode);
+            if (model.Id > 0)
+            {
+                #region 向缓存中加入linkCode
+                var linkCodesObj = CacheHelper.GetCache("FriendshipLinksControllerlinkCodes");
+                var linkCodes = linkCodesObj == null ? "": linkCodesObj.ToString();
+                if (string.IsNullOrEmpty(linkCodes))
+                {
+                    CacheHelper.AddCache("FriendshipLinksControllerlinkCodes", linkCode + ",");
+                }
+                else if (linkCodes.IndexOf(linkCode) == -1)
+                {
+                    CacheHelper.SetCache("FriendshipLinksControllerlinkCodes", linkCodes + linkCode + ",");
+                }
+                #endregion
+
+                if (type == "ip")
+                {
+                    var endDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 23, 59, 59);
+
+                    var ipsObj = CacheHelper.GetCache("FriendshipLinksControllerIp" + linkCode);
+                    var ips = ipsObj == null ? "" : ipsObj.ToString();
+                    if (string.IsNullOrEmpty(ips) || ips.IndexOf("," + ip + ",") == -1)
+                    {
+                        if (string.IsNullOrEmpty(ips))
+                        {
+                            CacheHelper.AddCache("FriendshipLinksControllerIp" + linkCode, "," + ip + ",", endDate);
+                        }
+                        else
+                        {
+                            CacheHelper.SetCache("FriendshipLinksControllerIp" + linkCode, ips + ip + ",", endDate);
+                        }
+
+                        _service.AddIp(model.Id);
+                    }
+                }
+                if (type == "uv")
+                {
+                    #region 向缓存中增加PV数
+                    lock (objUv)
+                    {
+                        var obj = CacheHelper.GetCache("FriendshipLinksControllerUv" + linkCode);
+                        if (obj == null)
+                        {
+                            CacheHelper.AddCache("FriendshipLinksControllerUv" + linkCode, 1);
+                        }
+                        else
+                        {
+                            var uvNum = Convert.ToInt32(CacheHelper.GetCache("FriendshipLinksControllerUv" + linkCode));
+                            CacheHelper.SetCache("FriendshipLinksControllerUv" + linkCode, uvNum + 1);
+                        }
+                    }
+                    #endregion
+                }
+                if (type == "pv")
+                {
+                    #region 向缓存中增加PV数
+                    lock (objPv)
+                    {
+                        var obj = CacheHelper.GetCache("FriendshipLinksControllerPv" + linkCode);
+                        if (obj == null)
+                        {
+                            CacheHelper.AddCache("FriendshipLinksControllerPv" + linkCode, 1);
+                        }
+                        else
+                        {
+                            var pvNum = Convert.ToInt32(CacheHelper.GetCache("FriendshipLinksControllerPv" + linkCode));
+                            CacheHelper.SetCache("FriendshipLinksControllerPv" + linkCode, pvNum + 1);
+                        }
+                    }
+                    #endregion
+                }
+            }
+        }
+
+        /// <summary>
+        /// 添加PV UV
+        /// </summary>
+        /// <param name="state"></param>
+        private void AddPvUv(object state)
+        {
+            var linkCodesObj = CacheHelper.GetCache("FriendshipLinksControllerlinkCodes");
+            var linkCodes = linkCodesObj == null ? "" : linkCodesObj.ToString();
+            if (!string.IsNullOrEmpty(linkCodes))
+            {
+                var linkCodeArray = linkCodes.Split(',');
+                foreach (var linkCode in linkCodeArray)
+                {
+                    if (string.IsNullOrEmpty(linkCode))
+                        continue;
+
+                    FriendLink model = GetFriendLink(linkCode);
+                    if (model.Id > 0)
+                    {
+                        #region 保存PV
+                        var pvCount = 0;
+                        lock (objPv)
+                        {
+                            pvCount = Convert.ToInt32(CacheHelper.GetCache("FriendshipLinksControllerPv" + linkCode));
+                            CacheHelper.SetCache("FriendshipLinksControllerPv" + linkCode, 0);
+                        }
+
+                        if (pvCount != 0)
+                        {
+                            _service.AddPv(model.Id, pvCount);
+                        }
+                        #endregion
+
+                        #region 保存UV
+                        var uvCount = 0;
+                        lock (objUv)
+                        {
+                            uvCount = Convert.ToInt32(CacheHelper.GetCache("FriendshipLinksControllerUv" + linkCode));
+                            CacheHelper.SetCache("FriendshipLinksControllerUv" + linkCode, 0);
+                        }
+
+                        if (uvCount != 0)
+                        {
+                            _service.AddUv(model.Id, uvCount);
+                        }
+                        #endregion
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// 获取友情链接对象
+        /// </summary>
+        /// <param name="linkCode"></param>
+        /// <returns></returns>
+        private FriendLink GetFriendLink(string linkCode)
+        {
+            List<FriendLink> list = null;
+            var friendLinkList = CacheHelper.GetCache("FriendshipLinksControllerFriendLinkList");
+            if (friendLinkList == null)
+            {
+                list = _service.GetFriendLink();
+
+                CacheHelper.AddCache("FriendshipLinksControllerFriendLinkList", list, new DateTime().AddHours(2));
+            }
+            else
+            {
+                list = friendLinkList as List<FriendLink>;
+            }
+
+            return list.FirstOrDefault(e => e.Code == linkCode);
+        }
+    }
+}
