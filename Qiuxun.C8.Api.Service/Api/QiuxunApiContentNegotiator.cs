7dd1eff4923fbba1f66f4f9f97655c4865273003
Luohongshun <453761305@qq.com>
2018-04-26 10:12:28 +0800

    发帖接口新增封盘

--- Qiuxun.C8.Api.Service/Api/QiuxunApiContentNegotiator.cs
+++ Qiuxun.C8.Api.Service/Api/QiuxunApiContentNegotiator.cs
@@ -1,11 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Formatting;
 using System.Net.Http.Headers;
-using System.Text;
-using System.Threading.Tasks;
 using Qiuxun.C8.Api.Service.Api.Securities;
 using Qiuxun.C8.Api.Service.Common;
 
